d441b004f266e09c3a492c3a21830b124317f181
Samuel Richey <samrichey01@gmail.com>
2026-03-06 19:38:02 -0700

    Updated eda_1.R

--- 14 - Model Workflow Day 1 (in class).R
+++ 14 - Model Workflow Day 1 (in class).R
@@ -2,23 +2,34 @@ library(tidyverse)
 library(tidymodels)
 
 # Okay let's work through the (cleaned) titanic data from last week:
-titanic <- read_csv('https://www.dropbox.com/s/92funarubgk5rzh/titanic_clean.csv?dl=1')
+titanic_raw <- read_csv('https://www.dropbox.com/s/92funarubgk5rzh/titanic_clean.csv?dl=1')
 
 
 # First let's make sure factors are factors
-
-
+titanic <- titanic_raw |> 
+  mutate(survived = as_factor(survived),
+         had_cabin = as_factor(had_cabin),
+         pclass = as_factor(pclass))
 
 
 # Now let's do a train/test split
+leo_split <- initial_split(
+  data = titanic,
+  prop = .75,
+  strata = survived
+)
+leo_split
 
 
-
+leo_training <- leo_split |> training()
+leo_testing <- leo_split |>  testing()
 
 # Plan the model setup, including the engine and mode
+leo_spec <- logistic_reg() |>
+  set_engine('glm') |> 
+  set_mode('classification')
 
-
-
+show_engines('logistic_reg')
 # relevant model types: logistic_reg(), linear_reg(), decision_tree(), rand_forest(), boost_tree()
 # show_engines('logistic_reg')
 
@@ -26,7 +37,9 @@ titanic <- read_csv('https://www.dropbox.com/s/92funarubgk5rzh/titanic_clean.csv
 
 
 # Now fit a model, look at output with tidy()
-
+leo_spec |> 
+  fit(survived ~ .,
+      data = leo_training)
 
 
 
